04a7956729be4927fb17c8f3fd7becd08f89677e
amardeepparhar <amardeepparhar@hotmail.com>
2017-06-12 17:12:46 -0700

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.IOException;
+/**
+ * Fibonacci Sequence Calculator
+ * @author amardeepparhar
+ * 	
+ * Description: Enter a number and have the program generate the Fibonacci number.
+ */
+public class fibonacci {
+
+	public static void main(String args[]) throws IOException{
+		Scanner scan = new Scanner(System.in);
+			System.out.println("Enter a integer : ");
+			int num = scan.nextInt();
+			
+			System.out.println("Number: "+fibRecusion(num));		
+	}
+	
+	private static int fibRecusion(int num){
+		if(num == 1 || num == 2){
+			return 1;
+		}
+		else{
+			return fibRecusion(num-1)+fibRecusion(num-2);
+		}
+	}
+}
